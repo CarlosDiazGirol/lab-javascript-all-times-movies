bc5f99fc91c4ae2f351f95e729cabb771d24b9b8
CarlosDiazGirol <carlosdiazgirol@hotmail.com>
2018-11-09 20:16:14 +0100

    minutes ok but no yasmineç

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,6 +2,28 @@
 // Turn duration of the movies from hours to minutes 
 
 
+function turnHoursToMinutes() {
+  var newMovies = movies.map(function (newObj) {
+    var newTime = Object.assign({}, newObj);
+    return newTime;
+  });
+  for (i = 0; i < newMovies.length; i++) {
+    newMovies[i].duration = newMovies[i].duration.replace(/h|min/g,'').split(' ');
+    if (newMovies[i].duration.length === 2) {
+      var hours = parseInt(newMovies[i].duration[0], 10);
+      var min = parseInt(newMovies[i].duration[1], 10);
+      newMovies[i].duration = [hours, min];
+      newMovies[i].duration = hours * 60 + min;
+    } else {
+      var min = parseInt(newMovies[i].duration[0], 10);
+      newMovies[i].duration = [min];
+      newMovies[i].duration = min;
+    }
+  }
+  return newMovies;
+}
+
+
 // Get the average of all rates with 2 decimals 
 
 
